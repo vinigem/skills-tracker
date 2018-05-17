373b82506a982a83b4cfbec7c3dac9579561301e
Vinit Kumar <Kumar_Vinit@cat.com>
2018-05-17 17:39:25 +0530

    First commit

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddEmployeeComponent } from './employee/add-employee.component';
+import { UpdateEmployeeComponent } from './employee/update-employee.component';
+import { AddSkillComponent } from './skill/add-skill.component';
+
 const routes: Routes = [
-    
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'add-employee', component: AddEmployeeComponent }, 
+    { path: 'update-employee', component: UpdateEmployeeComponent },
+    { path: 'add-skills', component: AddSkillComponent }  
 ];
 
 
