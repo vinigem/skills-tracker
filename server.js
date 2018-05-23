73665acd2c257a8ed1979a36af77d85193a1b367
Vinit Kumar <Kumar_Vinit@cat.com>
2018-05-23 16:58:07 +0530

    all functions done

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
-var express = require('express'),
-    app = express();
-    app.use(express.static('www'));
-    app.set('port', process.env.PORT || 5000);
-    app.listen(app.get('port'), function () {
-        console.log('Express server listening on port ' + app.get('port'));
-    });
+const path = require('path');
+const express = require('express');
+const app = express();
+    
+app.use(express.static(__dirname + '/dist'));
+app.set('port', process.env.PORT || 5000);
+   
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+// Instruct the app to use the forceSSL middleware
+app.use(forceSSL());
